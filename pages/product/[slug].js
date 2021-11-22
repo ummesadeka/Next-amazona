c4318c6aaaf021fa1af9ae14db74b4518a6b54b4
ummesadeka <sadekaahmed53@gmail.com>
2021-11-22 17:41:42 +0600

    Fetch Products From API

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,24 +7,23 @@ import {
     Card,
     Button,
   } from '@material-ui/core';
-import { useRouter } from 'next/router'
 import React from 'react'
 import Layout from '../../components/Layout';
-import data from '../../utils/data';
 import NextLink from 'next/link';
 import useStyles from '../../utils/styles';
 import Image from 'next/image';
+import db from '../../utils/db';
+import Product from '../../models/Product';
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const { product } = props;
     const classes = useStyles();
-    const router = useRouter();
-    const { slug } =  router.query;
-    const product = data.products.find((a) => a.slug === slug);
+    
     if (!product) {
         return <div>Product not found</div>;
     }
     return (
-        <Layout title={product.name} description={product.desciption}>
+        <Layout title={product.name} description={product.description}>
             <div className={classes.section}>
                 <NextLink href="/" passHref>
                     <Link>
@@ -100,3 +99,17 @@ export default function ProductScreen() {
         </Layout>
     )
 }
+
+export async function getServerSideProps(context) {
+    const { params } = context;
+    const { slug } = params;
+  
+    await db.connect();
+    const product = await Product.findOne({ slug }).lean();
+    await db.disconnect();
+    return {
+      props: {
+        product: db.convertDocToObj(product),
+      },
+    };
+  }
