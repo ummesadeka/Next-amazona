1474165e70c6d7b3303bed792d4337b3a87606e3
ummesadeka <sadekaahmed53@gmail.com>
2021-11-28 19:20:55 +0600

    Dynamic Import Components fix

--- pages/cart.js
+++ pages/cart.js
@@ -3,6 +3,7 @@ import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import NextLink from 'next/link';
 import Image from 'next/image';
+import dynamic from 'next/dynamic';
 import {
   Grid,
   TableContainer,
@@ -21,7 +22,7 @@ import {
   ListItem,
 } from '@material-ui/core';
 
-export default function CartScreen() {
+ function CartScreen() {
   const { state } = useContext(Store);
   const {
     cart: { cartItems },
@@ -117,3 +118,5 @@ export default function CartScreen() {
     </Layout>
   );
 }
+
+export default dynamic(()=> Promise.resolve(CartScreen), { ssr: false} );
